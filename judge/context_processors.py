5776aa86b88d1ff14058cea0cc4f78822b98bc2c
Zap <67962871+proZap@users.noreply.github.com>
2021-05-24 21:29:42 +0700

    Fix linting error

--- judge/context_processors.py
+++ judge/context_processors.py
@@ -1,4 +1,5 @@
 from django.utils import timezone
+
 from judge.models import Contest
 
 
--- judge/views/blog.py
+++ judge/views/blog.py
@@ -7,8 +7,8 @@ from django.utils.translation import ugettext as _
 from django.views.generic import ListView
 
 from judge.comments import CommentedDetailView
-from judge.models import BlogPost, Comment, Contest, Language, Problem, ProblemClarification, Profile, Submission, \
-    Ticket, ProblemType, ProblemGroup
+from judge.models import BlogPost, Comment, Contest, Language, Problem, ProblemClarification, ProblemGroup, ProblemType, \
+    Profile, Submission, Ticket
 from judge.utils.cachedict import CacheDict
 from judge.utils.diggpaginator import DiggPaginator
 from judge.utils.problems import user_completed_ids
